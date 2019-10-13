086811d6079404d93899f35d5f664a5d109a726f
Demys Lima <demys.lima@contabilizei.com.br>
2019-10-13 16:57:07 -0300

    automatizando a criação de rotas em Vue.js

--- src/router.js
+++ src/router.js
@@ -4,6 +4,27 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
+const generateRoute = componentName => {
+  return {
+    path: `/${componentName.toLowerCase()}`,
+    name: componentName.toLowerCase(),
+    // webpackChunkName: "[request]" o [request] será substituido por ([componentName]-vue)
+    // o arquivo final será ([componentName]-vue.[hash].js) ex: About-vue-91es1946.js
+    component: () => import(/* webpackChunkName: "[request]" */ `./views/${componentName}.vue`)
+  }
+}
+
+function loadRoutes () {
+  // queremos apenas o nome do component e o keys() irá retorna tanto ./About quanto ./About.vue
+  // então colocamos um regex informado que não queremos os arquivos que contém vue no nome
+  // e no último paramêtro colocamos um lazy para informa que vai ser feito o carregamento por demanda
+  const response = require.context('./views', true, /^((?!vue).)*$/, 'lazy')
+  return response.keys()
+    .map(item => item.replace('./', '')) // removendo o './'  dos nomes
+    .filter(item => item !== 'Home') // removendo o Home da lista, pois ela será a rota principal
+    .map(generateRoute) // gerando um array de Route
+}
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -13,13 +34,6 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    ...loadRoutes()
   ]
 })
